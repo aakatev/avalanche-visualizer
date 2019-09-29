fe8ca0eee48c99602f5f258c93cf316db69bfe27
aakatev <aakatev@mail.sfsu.edu>
2019-09-28 22:44:00 -0700

    Some updates

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ async function sha256(message) {
   return hashHex;
 }
 
-sha256("lol").then(hash => {
+sha256("A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.").then(hash => {
   let hashCharsHex = hash.split("");
   let hashCharsDec = hashCharsHex.map(hexChar => parseInt(hexChar, 16));
   let charIndexies = []
@@ -22,8 +22,8 @@ sha256("lol").then(hash => {
   }
   // console.log(hashCharsDec);
 
-  let ctx = document.getElementById('myChart').getContext('2d');
-  let myChart = new Chart(ctx, {
+  let ctx1 = document.getElementById('myChart1').getContext('2d');
+  let myChart1 = new Chart(ctx1, {
     type: 'line',
     data: {
       labels: charIndexies,
@@ -43,3 +43,33 @@ sha256("lol").then(hash => {
   });
 })
 
+
+sha256(" paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.").then(hash => {
+  let hashCharsHex = hash.split("");
+  let hashCharsDec = hashCharsHex.map(hexChar => parseInt(hexChar, 16));
+  let charIndexies = []
+  for(let i = 0; i < 64; i++) {
+    charIndexies[i] = i;
+  }
+  // console.log(hashCharsDec);
+
+  let ctx2 = document.getElementById('myChart2').getContext('2d');
+  let myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+      labels: charIndexies,
+      datasets: [{ 
+          data: hashCharsDec,
+          label: "Characters",
+          borderColor: "#3e95cd"
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'avalanche effect visualizer (sha256)'
+      }
+    }
+  });
+})
